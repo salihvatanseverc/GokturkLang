0395083562b65001159842c0ffe686ee113e606a
Salih <gokturkcom.bg@gmail.com>
2026-05-11 17:57:16 +0300

    GokturkStudio ilk sürüm

---
+++ GokturkStudio/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+
+    const win = new BrowserWindow({
+
+        width: 1400,
+        height: 900,
+
+        webPreferences: {
+
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+
+    createWindow();
+});
